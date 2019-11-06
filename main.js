c4d472a78a87a3c08d5e347945df66165af8d4c4
Ximena Pinacho <zbrm34@gmail.com>
2019-11-05 19:07:04 -0600

    agregué método para post del usuario

--- main.js
+++ main.js
@@ -3,9 +3,26 @@
 // Librerias (solo se importan librerias que esten en contacto con el usurario)
 const express = require('express'); // porque va a estar usando esta libreria (express)
 const colors = require('colors');
+const bodyParser = require('body-parser');
 
 //Aplicación
 const app = express(); //la función express
+
+// configuración de la App
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*'); // permitir acceso sin importar la ubicación del usuario
+
+  res.header('Access-Control-Allow-Headers', 'Origin, X-request-With, Content-Type, Accept, x-auth-token'); 
+  // Permitir que cualquier usurario acceda a la información sin que este autentificado
+
+  res.header('Access-Control-Expose-Headers', 'x-auth-token'); // permitir ver esa información
+
+  next() // Para que salga del bucle
+});
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
 var port = 1234; //para correr el servidor
 
@@ -19,6 +36,12 @@ app.get('/hi', (req, res) => {
 
 app.get('/info', (req, res) => {
   res.send('This actually works!');
+});
+
+app.post('/saludo', (req, res) => {
+  var name = req.body.name;
+
+  res.send(`Hola ${name} un gusto conocerte`);
 });
 
 // Correr app
